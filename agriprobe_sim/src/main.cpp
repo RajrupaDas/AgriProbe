ec797dcb1d7fe55516f4b03cd4a7a12fddfc832e
RajrupaDas <rajrupadas6@gamil.com>
2025-08-12 20:23:32 +0530

    motor sim

---
+++ agriprobe_sim/src/main.cpp
@@ -0,0 +1,19 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <iostream>
+using namespace std;
+LOG_MODULE_REGISTER(soil_sampler, LOG_LEVEL_INF);
+
+// Simulated motion times (ms)
+constexpr int DRILL_DOWN_TIME=1200;
+constexpr int DRILL_UP_TIME=1500;
+constexpr int DRILL_ROTATE_TIME=1300;
+constexpr int PLATE_ROTATE_TIME=2000;
+
+// Simulated motor functions
+void drill_down(){LOG_INF("Drill moving down");k_msleep(DRILL_DOWN_TIME);}
+void drill_up(){LOG_INF("Drill moving up");k_msleep(DRILL_UP_TIME);}
+void drill_rotate_forward(){LOG_INF("Drill rotating forward");k_msleep(DRILL_ROTATE_TIME);}
+void drill_rotate_reverse(){LOG_INF("Drill rotating reverse");k_msleep(DRILL_ROTATE_TIME);}
+void plate_rotate_to_slot(int slot){LOG_INF("Rotating plate to slot %d",slot);k_msleep(PLATE_ROTATE_TIME);}
+
