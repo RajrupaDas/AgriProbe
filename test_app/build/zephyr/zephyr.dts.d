916b69150d78808347650171d50873d31f00817d
RajrupaDas <rajrupadas6@gamil.com>
2025-08-12 20:15:41 +0530

    CMake added

---
+++ test_app/build/zephyr/zephyr.dts.d
@@ -0,0 +1,5 @@
+empty_file.o: /home/quantumvortex/zephyrproject/zephyr/misc/empty_file.c \
+ /home/quantumvortex/zephyrproject/zephyr/boards/qemu/cortex_m3/qemu_cortex_m3.dts \
+ /home/quantumvortex/zephyrproject/zephyr/dts/arm/ti/lm3s6965.dtsi \
+ /home/quantumvortex/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/quantumvortex/zephyrproject/zephyr/dts/common/skeleton.dtsi
---
+++ test_app/src/main.c
@@ -0,0 +1,7 @@
+#include <zephyr/kernel.h>
+
+void main(void)
+{
+    printk("Zephyr says hello!\n");
+}
+
